c67b888926ba9c80263148145c66556f402d239b
Fazle Rabby <fazlerabby7@gmail.com>
2016-12-01 20:42:36 +0600

    Added WarnedUser model

--- warning_counter/models.py
+++ warning_counter/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class WarnedUser(models.Model):
+    # Facebook username or profile id of the warned user
+    user_id = models.CharField(max_length=50)
+    warning_count = models.IntegerField(default=1)
