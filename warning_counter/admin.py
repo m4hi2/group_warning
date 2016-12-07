4a5ff9516c17cfbeb5f85c4f24ba33a4d7f42e9c
Fazle Rabby <fazlerabby7@gmail.com>
2016-12-07 22:17:50 +0600

    Setting up admin site

--- warning_counter/admin.py
+++ warning_counter/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import WarnedUser
 
-# Register your models here.
+
+class WarnedUserAdmin(admin.ModelAdmin):
+    '''Django admin panel representation of WarnedUser model'''
+    list_display = ['user_id', 'warning_count']
+    list_filter = ['warning_count']
+    search_fields = ['user_id']
+
+    class Meta:
+        model = WarnedUser
+
+
+admin.site.register(WarnedUser, WarnedUserAdmin)
