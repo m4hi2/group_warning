306e141dc878c1ddd7b73856f4a8926cb10b9460
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2016-12-10 02:37:08 +0600

    reverted everything to old place

--- warning_counter/views.py
+++ warning_counter/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .forms import WarnedUserForm
 from .models import WarnedUser
 
-import re
 
 def home(request):
     '''
@@ -11,28 +10,15 @@ def home(request):
     or create WarnedUser object if it doesn't exist.
     '''
     warned_user = None
-    pattern = re.compile(r"(http|https):\/\/?(?:web.)?facebook.com\/.+\?+(fref)+=(nf|ufi)")
     if request.method == 'POST':
         form = WarnedUserForm(request.POST)
         if form.is_valid():
             user_id = form.cleaned_data['user_id']
+            print(type(user_id))
             try:
-                match = pattern.search(user_id)
-                if match:
-                    # TODO: Add a mechanism to seek out the username from the link.
-                    link = match.group()
-                    clean_link = link[25:]
-                    link_chars = []
-                    for i in clean_link:
-                        if i != '?':
-                            link_chars.append(i)
-                        else:
-                            break
-                    username = ''.join(link_chars)
-                else:
-                    username = user_id
-
-                warned_user = WarnedUser.objects.get(user_id=username)
+                warned_user = WarnedUser.objects.get(user_id=user_id)
+                print(username)
+                print('asdlfkj')
                 warned_user.warning_count += 1
                 warned_user.save()
             except WarnedUser.DoesNotExist:
