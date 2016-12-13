9816d04340d474b17ca875b10360c025b4e61afb
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2016-12-14 03:23:05 +0600

    Changed function name and replaced the loop with regex magic for faster and more reliable operations.

--- warning_counter/facebook.py
+++ warning_counter/facebook.py
@@ -4,23 +4,14 @@ The whole purpose in life of this file is to handle various stuff related to fac
 
 import re
 
-def is_facebook_link(link):
+def get_facebook_username(facebook_profile_link):
     '''
     This function uses the regex defined in 'pattern' variable to look for if the provided link is form a Facebook group feed.
     '''
-    pattern = re.compile(r"(http|https):\/\/?(?:web.)?facebook.com\/.+\?+(fref)+=(nf|ufi)")
-    match = pattern.search(link)
+    pattern = re.compile(r"(http|https):\/\/(web\.|www\.)facebook\.com\/(.+)\?fref=(nf|ufi)")
+    match = pattern.search(facebook_profile_link)
     if match:
-        raw_link = match.group()
-        # With HTTPS the part before the username is 25chars long.
-        username_with_excess = raw_link[25:]
-        username_chars = []
 
-        # Loops through the string to find the username chars.
-        for char in username_with_excess:
-            if char != '?':
-                username_chars.append(char)
-            else:
-                break
-        username = ''.join(username_chars)
+        # RegEx magic, baby ;)
+        username = match.group(3)
         return username
