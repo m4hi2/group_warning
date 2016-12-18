3e7dfa1edd92cb60d1eb8fe2f4ec8ab4a4e7980b
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2016-12-19 03:38:34 +0600

    Updated RegEx for compatibility. Added support for extracting profile ID form a profile link.

--- warning_counter/facebook.py
+++ warning_counter/facebook.py
@@ -8,10 +8,22 @@ def get_facebook_username(facebook_profile_link):
     '''
     This function uses the regex defined in 'pattern' variable to look for if the provided link is form a Facebook group feed.
     '''
-    pattern = re.compile(r"(http|https):\/\/(web\.|www\.)facebook\.com\/(.+)\?fref=(nf|ufi)")
+    pattern = re.compile(r"(http|https):\/\/(web|www|m|mobile|touch|mbasic|iphone|free)\.facebook\.com\/(.+)\?fref=(nf|ufi)")
     match = pattern.search(facebook_profile_link)
     if match:
 
         # RegEx magic, baby ;)
         username = match.group(3)
         return username
+
+def get_facebook_profile_ID(facebook_profile_link):
+    '''
+    :param facebook_profile_link: provided facebook profile link
+    :return: facebook profile ID
+    '''
+    pattern = re.compile(r"(http|https):\/\/(web|www|m|mobile|touch|mbasic|iphone|free)\.facebook\.com\/profile\.php\?id=([0-9]{15})")
+    match = pattern.search(facebook_profile_link)
+    if match:
+        # regex group
+        user_id = match.group(3)
+        return user_id
