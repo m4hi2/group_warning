831d2277a89d450445a6715209b8516ab66de915
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2016-12-20 05:11:54 +0600

    Merged get_id and get_username function with slithly well thought (Spoiler: Not well structured) regex pattern

--- warning_counter/facebook.py
+++ warning_counter/facebook.py
@@ -4,26 +4,22 @@ The whole purpose in life of this file is to handle various stuff related to fac
 
 import re
 
-def get_facebook_username(facebook_profile_link):
+
+def get_facebook_username_or_id(facebook_profile_link):
     '''
     This function uses the regex defined in 'pattern' variable to look for if the provided link is form a Facebook group feed.
     '''
-    pattern = re.compile(r"(http|https):\/\/(web|www|m|mobile|touch|mbasic|iphone|free)\.facebook\.com\/(.+)\?fref=(nf|ufi)")
+
+    pattern = re.compile(r"(http|https):\/\/(web|www|m|mobile|touch|mbasic|iphone|free)\.facebook\.com\/((.+)\?fref=(nf|ufi)|profile\.php\?id=([0-9]{15}))")
     match = pattern.search(facebook_profile_link)
     if match:
 
         # RegEx magic, baby ;)
-        username = match.group(3)
-        return username
-
-def get_facebook_profile_ID(facebook_profile_link):
-    '''
-    :param facebook_profile_link: provided facebook profile link
-    :return: facebook profile ID
-    '''
-    pattern = re.compile(r"(http|https):\/\/(web|www|m|mobile|touch|mbasic|iphone|free)\.facebook\.com\/profile\.php\?id=([0-9]{15})")
-    match = pattern.search(facebook_profile_link)
-    if match:
-        # regex group
-        user_id = match.group(3)
-        return user_id
+        if match.group(4):
+            username = match.group(4)
+            return username
+        elif match.group(6):
+            userid = match.group(6)
+            return userid
+        else:
+            return None
